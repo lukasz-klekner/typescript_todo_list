756ccd9592a630629ad7a2bebf7f6a5a3a1b4680
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-06 18:35:12 +0100

    adds state to app and types props for InputField

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './App.css'
 import InputField from './components/InputField'
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState('')
+
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   )
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,20 @@
 import './style.css'
 
-const InputField = () => {
+interface Props {
+  todo: string
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
   return (
     <form className='input'>
-      <input type='text' placeholder='Enter a Todo' className='input__box' />
+      <input
+        type='text'
+        placeholder='Enter a Todo'
+        className='input__box'
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button type='submit' className='input__submit'>
         GO
       </button>
