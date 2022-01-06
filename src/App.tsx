5f00cdf9520ae8d3de20de52abec86b63d9fa274
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-06 15:50:50 +0100

    refactors App and adds some styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 import './App.css'
 
-function App() {
-  return <div className='App'>Hello world!</div>
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <span className='heading'>Taskify</span>
+    </div>
+  )
 }
 
 export default App
